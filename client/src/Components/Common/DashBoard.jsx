7ac8eb8ea88df3997d4564fec082165483d9ae26
unknown <migaraten@gmail.com>
2024-02-16 10:34:29 +0200

    edit dashboard

--- client/src/Components/Common/DashBoard.jsx
+++ client/src/Components/Common/DashBoard.jsx
@@ -4,31 +4,29 @@ import axios from "axios";
 import NavBar from "./NavBar";
 import SideBar from "./SideBar";
 
-
 const DashBoard = () => {
   return (
-    <div className="flex">
-  {/* Sidebar */}
-  <SideBar />
+    <div className="flex h-screen">
+      {/* Sidebar */}
+      <SideBar />
 
-  <div className="grow flex flex-col overflow-hidden p-3 pb-[3px]">
-    {/* Navbar */}
-    <NavBar />
+      <div className="grow flex flex-col overflow-hidden p-3 pb-[3px]">
+        {/* Navbar */}
+        <NavBar />
 
-    {/* Main Content */}
-    <div className="grow overflow-x-hidden overflow-y-auto bg-stone-300 mt-3 rounded-[5px] mb-0">
-      {/* Content Wrapper */}
-      <div className="py-6">
-        {/* Main Body */}
-        <div className="container mx-auto">
-          <Outlet />
+        {/* Main Content */}
+        <div className="grow overflow-x-hidden overflow-y-auto bg-stone-300 mt-3 rounded-[5px] mb-0">
+          {/* Content Wrapper */}
+          <div className="py-6">
+            {/* Main Body */}
+            <div className="container mx-auto">
+              <Outlet />
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
-
   );
 };
 
-export default DashBoard
+export default DashBoard;
