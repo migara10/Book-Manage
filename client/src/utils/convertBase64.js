70c628f28dcfa3f1c880564b638a8fa703c0553b
unknown <migaraten@gmail.com>
2024-02-17 03:31:44 +0200

    add image convert base64 function in util.js

---
+++ client/src/utils/convertBase64.js
@@ -0,0 +1,18 @@
+// convert Image base64
+
+function convertBase64(file){
+    return new Promise((resolve, reject) => {
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(file)
+
+        fileReader.onload = () => {
+            resolve(fileReader.result)
+        }
+
+        fileReader.onerror = (error) => {
+            reject(error)
+        }
+    })
+}
+
+export default convertBase64;
